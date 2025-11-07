c177ab378230fe1f8eecfa739c32883bea972e73
Anandhu Udayan <anandhuudayan180@gmail.com>
2025-11-07 12:52:51 +0530

    feat: add date formatting and integrate real transaction list in UI

--- lib/screens/transactions/screen_transactions.dart
+++ lib/screens/transactions/screen_transactions.dart
@@ -1,32 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:money_manager/db_functions/transactions_db/transaction_db.dart';
+import 'package:money_manager/models/transactions_model/transaction_model.dart';
 
 class ScreenTransactions extends StatelessWidget {
   const ScreenTransactions({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: EdgeInsets.all(10),
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            leading: CircleAvatar(
-              radius: 38,
-              child: Text(
-                'Oct\n4',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontFamily: 'Montserrat'),
-              ),
-            ),
-            title: Text('RS 1000'),
-            subtitle: Text('income'),
-          ),
-        );
-      },
-      separatorBuilder: (ctx, index) {
-        return SizedBox(height: 10);
-      },
-      itemCount: 30,
+    return ValueListenableBuilder(
+      valueListenable: TransactionDb().newTransactionListNotifier,
+      builder:
+          (BuildContext context, List<TransactionModel> transactions, child) {
+            return ListView.separated(
+              padding: EdgeInsets.all(10),
+              itemBuilder: (context, index) {
+                final newTransaction = transactions[index];
+                String date = formatDate(newTransaction);
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 38,
+                      child: Text(
+                        date,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontFamily: 'Montserrat'),
+                      ),
+                    ),
+                    title: Text('RS. ${newTransaction.amount}'),
+                    subtitle: Text(newTransaction.category.name),
+                    trailing: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.delete, color: Colors.red),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (ctx, index) {
+                return SizedBox(height: 10);
+              },
+              itemCount: transactions.length,
+            );
+          },
     );
+  }
+
+  String formatDate(TransactionModel newTransaction) {
+    final dateTime = newTransaction.date.toString();
+    final splitedDate = dateTime.split(' ')[0];
+    final DateTime date = DateTime.parse(splitedDate);
+    final format = DateFormat().add_MMMd();
+    final formattedDate = format.format(date).split(' ');
+
+    return '${formattedDate.last}\n${formattedDate.first}';
   }
 }
